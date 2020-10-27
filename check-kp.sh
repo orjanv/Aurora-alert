72f55ce6b7c772de163db83db3bb8a1f9b1e39a6
Ørjan Vøllestad <orjan@hoyd.net>
2020-10-27 20:23:22 +0100

    minor changes to bash script

--- check-kp.sh
+++ check-kp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Script to be used in crontab
-# part of https://gitlab.com/orjanv/Aurora-alert
-# 
+# Script to be used in crontab, requires mailutils
+# part of https://github.com/orjanv/Aurora-alert
+#
 OUT=$(/usr/bin/aurora-forecast -f)
 if (( $OUT )); then
     echo $OUT | mailx -s "Aurora warning" EMAILADDRESS;
